d2d1f40eaf679dc7c8d323875685d3e8971a3870
Ian Pansensoy <ipansensoy@gmail.com>
2017-03-24 19:33:42 +0800

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 $(function() {
+  var wWidth = $(window).width();
   var owl = $('.owl-carousel');
   owl.owlCarousel({
     nav: false,
@@ -16,4 +17,41 @@ $(function() {
   });
 
 
+
+  //smoothscroll
+  var sections = $('.section'),
+    nav = $('.nav'),
+    navHeight = nav.outerHeight();
+  if (wWidth >= 1200) {
+    $(window).on('scroll', function() {
+      var curPos = $(this).scrollTop();
+
+      sections.each(function() {
+        var top = $(this).offset().top - navHeight,
+          bottom = top + $(this).outerHeight();
+
+        if (curPos >= top && curPos <= bottom) {
+          nav.find('li').removeClass('active');
+          sections.removeClass('active');
+
+          $(this).addClass('active');
+          nav.find('a[href="#' + $(this).attr('id') + '"]').parent().addClass('active');
+          window.location.hash = $(this).attr('id');
+
+        }
+      });
+    });
+
+  }
+  $('a').on('click', function() {
+    nav.find('li').removeClass('active');
+    var $el = $(this),
+      id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - 128
+    }, 500);
+
+    return false;
+  });
 });
